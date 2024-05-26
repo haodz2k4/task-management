da80de9b98286190fa01d06de946340c9ab8aa8c
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-05-26 16:28:05 +0700

    implement pagination feature

--- v1/controller/task.controller.js
+++ v1/controller/task.controller.js
@@ -5,10 +5,31 @@ module.exports.index = async (req,res) =>{
     const find = {
         deleted: false
     }
+    //filter by status
     if(req.query.status){
         find.status = req.query.status
     }
-    const record = await tasks.find(find);
+    //end filter by status 
+    //pagination 
+    const objectPagination = {
+        limitPages: 5,
+        currentPages: 0
+    }
+    if(req.query.pages){
+        objectPagination.currentPages = parseInt(req.query.pages);
+        objectPagination.skip = objectPagination.limitPages * (objectPagination.currentPages - 1);
+    }
+    if(req.query.limit){
+        objectPagination.limitPages = req.query.limit;
+    }
+    //end pagination
+    //handle sorting
+    const objectSorting = {};
+    if(req.query.sortKey && req.query.sortValue){
+        objectSorting[req.query.sortKey] = req.query.sortValue;
+    }
+    //end sorting 
+    const record = await tasks.find(find).sort(objectSorting).limit(objectPagination.limitPages).skip(objectPagination.skip);
 
 
     res.json(record);
